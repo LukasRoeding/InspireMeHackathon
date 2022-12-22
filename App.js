2026e523265c564660b5277e2171cedc751b704c
xLorena <lb@bitsandlikes.de>
2022-12-22 12:16:41 +0100

    init

--- App.js
+++ App.js
@@ -1,25 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Configuration, OpenAIApi } from 'openai';
-import { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Configuration, OpenAIApi } from "openai";
+import { useState } from "react";
+import MainNavigator from "./navigation/navigation";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  MD3LightTheme as DefaultTheme,
+  Provider as PaperProvider,
+} from "react-native-paper";
 
 export default function App() {
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: "tomato",
+      secondary: "yellow",
+    },
+  };
   const configuration = new Configuration({
-    apiKey: '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',
+    apiKey: "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",
   });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <PaperProvider theme={theme}>
+        <View style={styles.container}>
+          <Text>Hallo</Text>
+          <MainNavigator />
+          <StatusBar style="auto" />
+        </View>
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
