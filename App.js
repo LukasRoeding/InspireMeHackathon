02043fe454e97936bfc146e6a9ac2d97415f004f
xLorena <lb@bitsandlikes.de>
2022-12-22 12:47:29 +0100

    refactor

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { Configuration, OpenAIApi } from "openai";
 import { useState } from "react";
-import { Button, TextInput } from "react-native";
 import "react-native-url-polyfill/auto";
-import MainNavigator from "./navigation/navigation";
-import { NavigationContainer } from "@react-navigation/native";
+import MainNavigator from "./navigation/Navigatior";
 import {
   MD3LightTheme as DefaultTheme,
   Provider as PaperProvider,
@@ -21,36 +18,13 @@ export default function App() {
       secondary: "yellow",
     },
   };
-  const [text, onChangeText] = React.useState(
-    "Write your text describing the image here."
-  );
-  const configuration = new Configuration({
-    apiKey: "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",
-  });
-  const openai = new OpenAIApi(configuration);
-  const generateImage = async () => {
-    try {
-      const res = await openai.createImage({
-        prompt: text,
-        n: 1,
-        size: "512x512",
-      });
-      console.log(res.data.data[0].url);
-    } catch (error) {
-      console.error(error);
-    }
-  };
   return (
-    <NavigationContainer>
-      <PaperProvider theme={theme}>
-        <View style={styles.container}>
-          <MainNavigator />
-          <TextInput onChangeText={onChangeText} value={text} />
-          <Button title="create Image" onPress={generateImage} />
-          <StatusBar style="auto" />
-        </View>
-      </PaperProvider>
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <MainNavigator />
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,38 @@
 //react-native
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import { Button, TextInput } from "react-native";
 //components
+import { Configuration, OpenAIApi } from "openai";
 import { Text } from "react-native-paper";
 
 const HomeScreen = (props) => {
   const styles = makeStyles();
+  const configuration = new Configuration({
+    apiKey: "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",
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const [text, onChangeText] = React.useState(
+    "Write your text describing the image here."
+  );
+  const generateImage = async () => {
+    try {
+      const res = await openai.createImage({
+        prompt: text,
+        n: 1,
+        size: "512x512",
+      });
+      console.log(res.data.data[0].url);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <View style={styles.screen}>
-      <Text>Hallo</Text>
-      <Text variant="displayLarge">HomeScreen</Text>
+      <TextInput onChangeText={onChangeText} value={text} />
+      <Button title="create Image" onPress={generateImage} />
     </View>
   );
 };
