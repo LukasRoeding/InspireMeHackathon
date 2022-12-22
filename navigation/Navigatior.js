77f95c37e802b628aa28dab09694f3f9d830e15d
xLorena <lb@bitsandlikes.de>
2022-12-22 13:18:53 +0100

    addes styling

--- navigation/Navigatior.js
+++ navigation/Navigatior.js
@@ -5,15 +5,44 @@ import SettingScreen from "../screens/SettingScreen";
 import ImageScreen from "../screens/ImageScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 const Tab = createMaterialBottomTabNavigator();
 
 const MainNavigator = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Texts" component={HomeScreen} />
-        <Tab.Screen name="Images" component={ImageScreen} />
-        <Tab.Screen name="Settings" component={SettingScreen} />
+        <Tab.Screen
+          name="Texts"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="text" color={color} size={26} />
+            ),
+          }}
+          component={HomeScreen}
+        />
+        <Tab.Screen
+          name="Images"
+          component={ImageScreen}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="image" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={SettingScreen}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="cog-outline"
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
