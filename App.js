0e2d3581c76491da8edda6338b9c97e7f0b99e4f
Lukas <goskodovesche@gmail.com>
2022-12-22 11:30:48 +0100

    configuration

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Configuration, OpenAIApi } from 'openai';
+import { useState } from 'react';
 
 export default function App() {
+  const configuration = new Configuration({
+    apiKey: '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',
+  });
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
